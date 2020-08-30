bb57aee9ac450563d6c153c4b0d5a056019f4b4d
Dmytro Kerest <demetrius2006@gmail.com>
2020-08-30 05:27:39 +0300

    runner script update

--- run.sh
+++ run.sh
@@ -3,12 +3,14 @@
 docker run -d --name="Zoneminder" \
 --net="bridge" \
 --privileged="true" \
+--restart=always \
 -p 8443:443/tcp \
--p 9000:9000/tcp \
+-p 9090:9000/tcp \
+-p 8080:80/tcp \
 -e TZ="Europe/Kiev" \
 -e SHMEM="512mb" \
 -e PUID="1000" \
--e PGID="100" \
+-e PGID="462" \
 -e INSTALL_HOOK="0" \
 -e INSTALL_FACE="0" \
 -e INSTALL_TINY_YOLOV3="0" \
@@ -17,6 +19,6 @@ docker run -d --name="Zoneminder" \
 -e INSTALL_YOLOV4="0" \
 -e MULTI_PORT_START="0" \
 -e MULTI_PORT_END="0" \
--v "./config":"/config":rw \
--v "./data":"/var/cache/zoneminder":rw \
+-v "/home/dima/zoneminder/config":"/config":rw \
+-v "/home/dima/zoneminder/data":"/var/cache/zoneminder":rw \
 dlandon/zoneminder
